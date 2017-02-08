20c761c5ee119330a948390f5a2a043d5a48e6b2
sergiorvs <segio.rolando@gmail.com>
2017-02-07 23:01:52 -0500

    ranking programadores
    falta una funcion que llene un vector con los temas seleccionados por el
    manager

--- demoapp/src/main/java/app/controlador/ManagerController.java
+++ demoapp/src/main/java/app/controlador/ManagerController.java
@@ -1,22 +1,104 @@
 package app.controlador;
 
+import java.util.List;
+
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import app.servicio.ManagerService;
+import antlr.collections.impl.Vector;
+import app.dominio.Theme;
+import app.dominio.UserProgmmr;
+import app.repositorio.ManagerRepository;
 
 @Controller
 public class ManagerController {
 
 	@Autowired
-	ManagerService managerService;
+	ManagerRepository managerrepository;
+	
+	
+	List<Long> th;
+	
 	
 	/*
 	@RequestMapping(value = "/manprojects", method = RequestMethod.GET)
 	String manprojects(ModelMap model){
 	return "manprojects";
 	}*/
+	
+	
+    Long promediar_array(Long[] a)
+	{
+		Long prom=0L;
+		for(int i=0;i<a.length;i++)
+		{
+			prom+=a[i];
+		}
+		return (prom/a.length);
+	}
+    
+    /*@RequestMapping(value = "/manuserproglist", method = RequestMethod.GET)
+    String solicitar_temas(ModelMap model)
+    {
+    	///falta agregar id de los temas al vector
+    	//for...
+    		//th.add(managerrepository.find_id_theme([arreglo con nombre de temas seleccionados][i]));
+    	return "manuserproglist";
+    }*/
+    
+	
+	@PostMapping("/man_prog_rank")
+	String mostrar_ranking_prog(ModelMap model)
+	{
+		th.add(1L);//momentario hasta que sepa como recolectar los temas
+		th.add(2L);//momentario hasta que sepa como recolectar los temas
+		List<UserProgmmr> prog = managerrepository.AllProgramers();
+		Long[] scoreaux = new Long[th.size()];
+		Long[] score = new Long[prog.size()];
+		//Long scpreaux[]
+		for(int i=0 ; i<prog.size() ; i++)
+		{
+			for(int j=0 ; j<th.size() ; j++)
+			{
+				scoreaux[j] = managerrepository.find_score_by_programmer(prog.get(i).getId() , th.get(i) );
+			}
+			score[i]=promediar_array(scoreaux);
+		}
+		/////////////////ordenando arreglos/////////////////////////////
+		
+		Long aux_score;
+		UserProgmmr aux_prog;
+		boolean v=false;
+		while(true)
+		{
+			v=false;
+			for(int i=1;i<score.length;i++)
+			{
+				if(score[i]>score[i-1])
+				{
+					aux_score=score[i];
+					aux_prog=prog.get(i);
+					
+					score[i]=score[i-1];
+					score[i-1]=aux_score;
+					
+					prog.set(i , prog.get(i-1));
+					prog.set(i-1, aux_prog);
+					v=true;
+				}
+			}
+			if(v==false) break;
+		}
+		
+		//////////////////////////////////////////////////////////////
+			
+		model.addAttribute("programer",prog);
+		model.addAttribute("score",score);
+		return "man_prog_rank";		
+	}
 }
--- demoapp/src/main/java/app/repositorio/ManagerRepository.java
+++ demoapp/src/main/java/app/repositorio/ManagerRepository.java
@@ -1,11 +1,15 @@
 package app.repositorio;
 
+
+import java.util.List;
+
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.Repository;
 import org.springframework.data.repository.query.Param;
 
 import app.dominio.Manager;
 import app.dominio.Person;
+import app.dominio.UserProgmmr;
 
 public interface ManagerRepository extends Repository<Manager,Long>{ 
 	Manager save(Manager manag);
@@ -13,11 +17,21 @@ public interface ManagerRepository extends Repository<Manager,Long>{
 	@Query(value = "SELECT * FROM Manager WHERE pers_id = ?1", nativeQuery = true)
 	Manager findManager(@Param("idpers")Long idpers);	
 	
+	//seleccionar todos los programadores
+	@Query(value = "SELECT * FROM user_progmmr",nativeQuery = true)
+	List<UserProgmmr>AllProgramers();
 	
+	//buscar el id del tema  (creo que no es necesario)
+	@Query(value="SELECT id FROM theme t WHERE t.nombre=?1", nativeQuery = true)
+	int find_id_theme(String nombre_tema);
 	
+	//buscar score del programador en un tema
+	@Query(value = "SELECT theme_score FROM user_prog_has_theme upt WHERE upt.user_progmmr_id=?1 and upt.theme_id=?2", nativeQuery=true)
+	Long find_score_by_programmer(Long id_prog,Long id_theme);
+	/*
 	//Task save(Task tarea); 
 	
-	/*
+	
 	///primero se deberia crear un projecto	
 		@Query("INSERT INTO Project VALUES(?1 , ?2 ,?3 ,?4)")
 		void crear_projecto(Long id,String descripcion,String nombre ,Boolean activo);
@@ -32,7 +46,7 @@ public interface ManagerRepository extends Repository<Manager,Long>{
 		///asignar tarea a un progrmador (FALTA...)
 		
 		@Query("SELECT m FROM Manager m ")
-		List<Manager> buscarTodos();
+		Collection<Manager> buscarTodos();
 		
 		@Query("SELECT m FROM Manager m WHERE m.id = :id")
 		Manager buscarPorId(@Param("id") Long id);
@@ -42,6 +56,6 @@ public interface ManagerRepository extends Repository<Manager,Long>{
 
 		@Query("SELECT M FROM Manager m WHERE m.id = :id_persona")
 		Manager BuscarPersonasPorId(@Param("id_persona") Long id_persona);
-	*/	
+	*/
 	
 }
